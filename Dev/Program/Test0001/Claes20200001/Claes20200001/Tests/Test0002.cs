2afa94533a1ffae416c28ffff8fa1213f0e74eec
soleil.taruto (cerulean.charlotte) <soleil.taruto@gmail.com>
2022-10-14 01:11:24 +0900

    ss

--- Dev/Program/Test0001/Claes20200001/Claes20200001/Tests/Test0002.cs
+++ Dev/Program/Test0001/Claes20200001/Claes20200001/Tests/Test0002.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Charlotte.Commons;
 
 namespace Charlotte.Tests
 {
@@ -9,7 +10,53 @@ namespace Charlotte.Tests
 	{
 		public void Test01()
 		{
-			// none
+			for (int c = 0; c < 20; c++)
+			{
+				Test01_a();
+			}
+		}
+
+		public void Test01_a()
+		{
+			const int TEST_COUNT = 10000;
+			int p = 0;
+
+			for (int testcnt = 0; testcnt < TEST_COUNT; testcnt++)
+			{
+				if (Test01_b())
+				{
+					p++;
+				}
+			}
+			Console.WriteLine(((double)p / TEST_COUNT).ToString("F9"));
+		}
+
+		private bool Test01_b()
+		{
+			bool[] seats = new bool[100];
+
+			seats[SCommon.CRandom.GetInt(100)] = true;
+
+			for (int i = 1; i < 99; i++)
+			{
+				if (seats[i])
+				{
+					int r;
+
+					do
+					{
+						r = SCommon.CRandom.GetInt(100);
+					}
+					while (seats[r]);
+
+					seats[r] = true;
+				}
+				else
+				{
+					seats[i] = true;
+				}
+			}
+			return !seats[99];
 		}
 	}
 }
