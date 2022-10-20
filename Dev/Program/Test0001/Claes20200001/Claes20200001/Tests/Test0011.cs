809976f74afae5631365a84dace1e787bc983a34
soleil.taruto (cerulean.charlotte) <soleil.taruto@gmail.com>
2022-10-20 19:04:19 +0900

    ss

--- Dev/Program/Test0001/Claes20200001/Claes20200001/Tests/Test0011.cs
+++ Dev/Program/Test0001/Claes20200001/Claes20200001/Tests/Test0011.cs
@@ -30,7 +30,7 @@ namespace Charlotte.Tests
 				.Select(v => new int[] { v })
 				.Test0011_ForEach(v => Console.WriteLine("1 : " + v[0]))
 				.Test0011_ForEach(v => v[0] *= -1)
-				.Test0011_ForEach(v => Console.WriteLine("2 : " + v[0])) // { -1, -2, -3 } にならない。-- { 1, 2, 3 } のまま。
+				.Test0011_ForEach(v => Console.WriteLine("2 : " + v[0])) // { -1, -2, -3 } にならない。-- { 1, 2, 3 } のまま。★分からん。
 				.DistinctOrderBy((a, b) => a[0] - b[0])
 				.Test0011_ForEach(v => Console.WriteLine("3 : " + v[0]))
 				.ToArray();
@@ -51,8 +51,30 @@ namespace Charlotte.Tests
 			Enumerable.Range(1, 3)
 				.Select(v => new int[] { v })
 				.Test0011_ForEach(v => Console.WriteLine("1 : " + v[0]))
-				.Test0011_ForEach(v => { v[0] *= -1; Console.WriteLine("2 : " + v[0]); }) // 3 : の前に表示されて { -1, -2, -3 } と表示されるけど ...
-				.Test0011_ForEach(v => Console.WriteLine("3 : " + v[0])) // { -1, -2, -3 } にならない。-- { 1, 2, 3 } のまま。
+				.Test0011_ForEach(v => { v[0] *= -1; Console.WriteLine("2 : " + v[0]); }) // 下の 3 : の前に表示されて { -1, -2, -3 } と表示されるけど ...
+				.Test0011_ForEach(v => Console.WriteLine("3 : " + v[0])) // { -1, -2, -3 } にならない。-- { 1, 2, 3 } のまま。★分からん。
+				.DistinctOrderBy((a, b) => a[0] - b[0])
+				.Test0011_ForEach(v => Console.WriteLine("4 : " + v[0]))
+				.ToArray();
+
+			// ----
+
+			Enumerable.Range(1, 3)
+				.Select(v => new int[] { v })
+				.Test0011_ForEach(v => Console.WriteLine("A : " + v[0]))
+				.ForEach(v => v[0] *= -1)
+				.ForEach(v => Console.WriteLine("B : " + v[0])) // ちゃんと { -1, -2, -3 } になる。
+				.DistinctOrderBy((a, b) => a[0] - b[0])
+				.Test0011_ForEach(v => Console.WriteLine("C : " + v[0]))
+				.ToArray();
+
+			// ----
+
+			Enumerable.Range(1, 3)
+				.Select(v => new int[] { v })
+				.Test0011_ForEach(v => Console.WriteLine("1 : " + v[0]))
+				.ForEach(v => { v[0] *= -1; Console.WriteLine("2 : " + v[0]); }) // 2 : -> 3 : と交互に表示される。{ -1, -2, -3 } と表示される。
+				.ForEach(v => Console.WriteLine("3 : " + v[0])) // ちゃんと { -1, -2, -3 } になる。
 				.DistinctOrderBy((a, b) => a[0] - b[0])
 				.Test0011_ForEach(v => Console.WriteLine("4 : " + v[0]))
 				.ToArray();
