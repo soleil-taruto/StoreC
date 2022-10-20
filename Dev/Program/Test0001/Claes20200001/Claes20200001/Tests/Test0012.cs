b5e5f951dffa8f53c70e9894d130f8889376495a
soleil.taruto (cerulean.charlotte) <soleil.taruto@gmail.com>
2022-10-21 07:31:09 +0900

    ss

--- Dev/Program/Test0001/Claes20200001/Claes20200001/Tests/Test0012.cs
+++ Dev/Program/Test0001/Claes20200001/Claes20200001/Tests/Test0012.cs
@@ -10,8 +10,6 @@ namespace Charlotte.Tests
 	{
 		public void Test01()
 		{
-			Console.WriteLine(Math.Tan(Math.PI * 0.5));
-
 			for (int degree = 0; degree < 360; degree++)
 			{
 				double rad = degree * (Math.PI / 180.0);
@@ -31,10 +29,13 @@ namespace Charlotte.Tests
 				//Console.WriteLine(degree + " ==> " + (s1 - s2).ToString("F20") + " , " + (c1 - c2).ToString("F20") + " , " + (t1 - t2).ToString("F20"));
 				Console.WriteLine(degree + " ==> " + Math.Abs(s1 - s2).ToString("F20") + " , " + Math.Abs(c1 - c2).ToString("F20") + " , " + Math.Abs(t1 - t2).ToString("F20"));
 			}
-
-			Console.WriteLine(MS_ValMin.ToString("F20"));
-			Console.WriteLine(MS_ValMax.ToString("F20"));
 		}
+
+		// ----
+		// ----
+		// ----
+
+		private const double PI = 3.14159265358979;
 
 		private double Tan(double rad)
 		{
@@ -43,32 +44,32 @@ namespace Charlotte.Tests
 
 		private double Sin(double rad)
 		{
-			return Cos(rad - Math.PI / 2.0);
+			return Cos(rad - PI / 2.0);
 		}
 
 		private double Cos(double rad)
 		{
 			while (rad < 0.0)
-				rad += Math.PI * 2.0;
+				rad += PI * 2.0;
 
-			while (Math.PI * 2.0 < rad)
-				rad -= Math.PI * 2.0;
+			while (PI * 2.0 < rad)
+				rad -= PI * 2.0;
 
 			return M_Cos(rad);
 		}
 
 		private double M_Cos(double rad)
 		{
-			if (Math.PI < rad)
-				return M_Cos(Math.PI * 2.0 - rad);
+			if (PI < rad)
+				return M_Cos(PI * 2.0 - rad);
 
-			if (Math.PI / 2.0 < rad)
-				return M_Cos(Math.PI - rad) * -1.0;
+			if (PI / 2.0 < rad)
+				return M_Cos(PI - rad) * -1.0;
 
 			D2Point lPt = new D2Point(1.0, 0.0);
 			D2Point rPt = new D2Point(0.0, 1.0);
 			double lRad = 0.0;
-			double rRad = Math.PI / 2.0;
+			double rRad = PI / 2.0;
 
 			for (int c = 0; c < 50; c++)
 			{
@@ -96,16 +97,10 @@ namespace Charlotte.Tests
 			}
 		}
 
-		private double MS_ValMin = double.MaxValue;
-		private double MS_ValMax = double.MinValue;
-
 		private double M_Sqrt(double value)
 		{
 			if (value < 0.0)
 				throw null; // Bad params
-
-			MS_ValMin = Math.Min(MS_ValMin, value);
-			MS_ValMax = Math.Max(MS_ValMax, value);
 
 			bool lessThenOne = value < 1.0;
 
