2b62f67c1a49007a2d4ab9e1a6e8bac367bd5059
soleil.taruto (cerulean.charlotte) <soleil.taruto@gmail.com>
2022-10-20 18:29:56 +0900

    ss

--- Dev/Program/Test0001/Claes20200001/Claes20200001/Tests/Test0011.cs
+++ Dev/Program/Test0001/Claes20200001/Claes20200001/Tests/Test0011.cs
@@ -29,10 +29,32 @@ namespace Charlotte.Tests
 			Enumerable.Range(1, 3)
 				.Select(v => new int[] { v })
 				.Test0011_ForEach(v => Console.WriteLine("1 : " + v[0]))
-				.Test0011_ForEach(v => v[0] *= -1) // <================================ !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! ???
-				.Test0011_ForEach(v => Console.WriteLine("2 : " + v[0]))
+				.Test0011_ForEach(v => v[0] *= -1)
+				.Test0011_ForEach(v => Console.WriteLine("2 : " + v[0])) // { -1, -2, -3 } にならない。-- { 1, 2, 3 } のまま。
 				.DistinctOrderBy((a, b) => a[0] - b[0])
 				.Test0011_ForEach(v => Console.WriteLine("3 : " + v[0]))
+				.ToArray();
+
+			// ----
+
+			Enumerable.Range(1, 3)
+				.Select(v => new int[] { v })
+				.Test0011_ForEach(v => Console.WriteLine("A : " + v[0]))
+				.ForEach(v => v[0] *= -1)
+				.Test0011_ForEach(v => Console.WriteLine("B : " + v[0])) // ちゃんと { -1, -2, -3 } になる。
+				.DistinctOrderBy((a, b) => a[0] - b[0])
+				.Test0011_ForEach(v => Console.WriteLine("C : " + v[0]))
+				.ToArray();
+
+			// ----
+
+			Enumerable.Range(1, 3)
+				.Select(v => new int[] { v })
+				.Test0011_ForEach(v => Console.WriteLine("1 : " + v[0]))
+				.Test0011_ForEach(v => { v[0] *= -1; Console.WriteLine("2 : " + v[0]); }) // 3 : の前に表示されて { -1, -2, -3 } と表示されるけど ...
+				.Test0011_ForEach(v => Console.WriteLine("3 : " + v[0])) // { -1, -2, -3 } にならない。-- { 1, 2, 3 } のまま。
+				.DistinctOrderBy((a, b) => a[0] - b[0])
+				.Test0011_ForEach(v => Console.WriteLine("4 : " + v[0]))
 				.ToArray();
 		}
 	}
