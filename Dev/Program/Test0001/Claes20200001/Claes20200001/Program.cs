364ff5a84d5e9a0efe1f286fd5ad6d2b3a90ae41
soleil.taruto (cerulean.charlotte) <soleil.taruto@gmail.com>
2022-10-26 06:01:31 +0900

    ss

--- Dev/Program/Test0001/Claes20200001/Claes20200001/Program.cs
+++ Dev/Program/Test0001/Claes20200001/Claes20200001/Program.cs
@@ -61,7 +61,8 @@ namespace Charlotte
 			//new Test0003().Test03();
 			//new Test0003().Test04();
 			//new Test0004().Test01();
-			new Test0004().Test02();
+			//new Test0004().Test02();
+			new Test0004().Test03();
 
 			// --
 		}
--- Dev/Program/Test0001/Claes20200001/Claes20200001/Tests/Test0004.cs
+++ Dev/Program/Test0001/Claes20200001/Claes20200001/Tests/Test0004.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Numerics;
+using Charlotte.Commons;
 
 namespace Charlotte.Tests
 {
@@ -21,23 +23,28 @@ namespace Charlotte.Tests
 
 			for (int n = 1; n < 1000000; n++)
 			{
-				if (n % 1000 == 0) Console.WriteLine(n); // cout
+				//if (n % 1000 == 0) Console.WriteLine(n); // cout
 
-				dict[Collatz(n)] = n;
+				int c = Collatz(n);
+
+				if (!dict.ContainsKey(c))
+					dict[c] = n;
+			}
+
+			foreach (int c in dict.Keys.OrderBy(SCommon.Comp))
+			{
+				Console.WriteLine(dict[c] + " --> " + c);
 			}
 		}
 
-		private int Collatz(int ni)
+		private int Collatz(BigInteger n)
 		{
-			ulong n = (ulong)ni;
 			int c;
 
 #if true
 			while (n % 2 == 0) n /= 2;
 			for (c = 0; n != 1; c++)
 			{
-				if (2642245 < n) throw null; // overflow
-
 				n = n * 3 + 1;
 				while (n % 2 == 0) n /= 2;
 			}
@@ -51,6 +58,19 @@ namespace Charlotte.Tests
 			}
 #endif
 			return c;
+		}
+
+		public void Test03()
+		{
+			BigInteger n = 837799;
+
+			while (n != 1)
+			{
+				Console.WriteLine(n);
+
+				n = n * 3 + 1;
+				while (n % 2 == 0) n /= 2;
+			}
 		}
 	}
 }
