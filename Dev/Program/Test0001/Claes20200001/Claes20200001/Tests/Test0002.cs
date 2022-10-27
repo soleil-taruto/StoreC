5624ed504cbffaf0185cdf58456be18b09ee0773
soleil.taruto (cerulean.charlotte) <soleil.taruto@gmail.com>
2022-10-27 18:17:03 +0900

    ss

---
+++ Dev/Program/Test0001/Claes20200001/Claes20200001/Tests/Test0002.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Charlotte.Commons;
+
+namespace Charlotte.Tests
+{
+	public class Test0002
+	{
+		public void Test01()
+		{
+			Console.WriteLine(SCommon.EraseExt("Test02"));
+			Console.WriteLine(SCommon.EraseExt("Test02.txt"));
+			Console.WriteLine(SCommon.EraseExt(@".\Test02"));
+			Console.WriteLine(SCommon.EraseExt(@".\Test02.txt"));
+			Console.WriteLine(SCommon.EraseExt(@"SubDir\Test02"));
+			Console.WriteLine(SCommon.EraseExt(@"SubDir\Test02.txt"));
+			Console.WriteLine(SCommon.EraseExt(@"\TopDir\Test02"));
+			Console.WriteLine(SCommon.EraseExt(@"\TopDir\Test02.txt"));
+			Console.WriteLine(SCommon.EraseExt(@"C:\TopDir\Test02"));
+			Console.WriteLine(SCommon.EraseExt(@"C:\TopDir\Test02.txt"));
+			Console.WriteLine(SCommon.EraseExt(@"\TopDir\SubDir\Test02"));
+			Console.WriteLine(SCommon.EraseExt(@"\TopDir\SubDir\Test02.txt"));
+			Console.WriteLine(SCommon.EraseExt(@"C:\TopDir\SubDir\Test02"));
+			Console.WriteLine(SCommon.EraseExt(@"C:\TopDir\SubDir\Test02.txt"));
+
+			/*
+			 * output ...
+Test02
+Test02
+.\Test02
+.\Test02
+SubDir\Test02
+SubDir\Test02
+\TopDir\Test02
+\TopDir\Test02
+C:\TopDir\Test02
+C:\TopDir\Test02
+\TopDir\SubDir\Test02
+\TopDir\SubDir\Test02
+C:\TopDir\SubDir\Test02
+C:\TopDir\SubDir\Test02
+Press ENTER key.
+
+			 * */
+
+		}
+	}
+}
