588c61565a70184b77900a0d941e045c140fb937
soleil.taruto (cerulean.charlotte) <soleil.taruto@gmail.com>
2022-10-27 22:28:21 +0900

    ss

--- Dev/Program/Test0001/Claes20200001/Claes20200001/Tests/Test0002.cs
+++ Dev/Program/Test0001/Claes20200001/Claes20200001/Tests/Test0002.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using Charlotte.Commons;
+using System.IO;
 
 namespace Charlotte.Tests
 {
@@ -25,26 +26,20 @@ namespace Charlotte.Tests
 			Console.WriteLine(SCommon.EraseExt(@"C:\TopDir\SubDir\Test02"));
 			Console.WriteLine(SCommon.EraseExt(@"C:\TopDir\SubDir\Test02.txt"));
 
-#if false // output
-
-Test02
-Test02
-.\Test02
-.\Test02
-SubDir\Test02
-SubDir\Test02
-\TopDir\Test02
-\TopDir\Test02
-C:\TopDir\Test02
-C:\TopDir\Test02
-\TopDir\SubDir\Test02
-\TopDir\SubDir\Test02
-C:\TopDir\SubDir\Test02
-C:\TopDir\SubDir\Test02
-Press ENTER key.
-
-#endif
-
+			Console.WriteLine(Path.GetExtension("Test02"));
+			Console.WriteLine(Path.GetExtension("Test02.txt"));
+			Console.WriteLine(Path.GetExtension(@".\Test02"));
+			Console.WriteLine(Path.GetExtension(@".\Test02.txt"));
+			Console.WriteLine(Path.GetExtension(@"SubDir\Test02"));
+			Console.WriteLine(Path.GetExtension(@"SubDir\Test02.txt"));
+			Console.WriteLine(Path.GetExtension(@"\TopDir\Test02"));
+			Console.WriteLine(Path.GetExtension(@"\TopDir\Test02.txt"));
+			Console.WriteLine(Path.GetExtension(@"C:\TopDir\Test02"));
+			Console.WriteLine(Path.GetExtension(@"C:\TopDir\Test02.txt"));
+			Console.WriteLine(Path.GetExtension(@"\TopDir\SubDir\Test02"));
+			Console.WriteLine(Path.GetExtension(@"\TopDir\SubDir\Test02.txt"));
+			Console.WriteLine(Path.GetExtension(@"C:\TopDir\SubDir\Test02"));
+			Console.WriteLine(Path.GetExtension(@"C:\TopDir\SubDir\Test02.txt"));
 		}
 	}
 }
