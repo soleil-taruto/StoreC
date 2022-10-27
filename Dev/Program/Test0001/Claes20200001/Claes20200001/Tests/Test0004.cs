108bd1a4a80c53e70b0954bae6f02a9d1c919366
soleil.taruto (cerulean.charlotte) <soleil.taruto@gmail.com>
2022-10-28 07:20:44 +0900

    ss

--- Dev/Program/Test0001/Claes20200001/Claes20200001/Tests/Test0004.cs
+++ Dev/Program/Test0001/Claes20200001/Claes20200001/Tests/Test0004.cs
@@ -10,9 +10,7 @@ namespace Charlotte.Tests
 	{
 		public void Test01()
 		{
-			RndCtr = (ulong)DateTime.Now.Ticks;
-
-			D2Point[] Points = Enumerable.Range(0, 91).Select(dummy => new D2Point(GetRandReal(), GetRandReal())).ToArray();
+			D2Point[] Points = Enumerable.Range(0, 91).Select(dummy => new D2Point(0.5, 0.5)).ToArray();
 
 			Points[0].X = 1.0;
 			Points[0].Y = 0.0;
@@ -20,18 +18,25 @@ namespace Charlotte.Tests
 			Points[90].X = 0.0;
 			Points[90].Y = 1.0;
 
-			for (int c = 0; c < 100000000; c++)
+			for (int c = 0; c < 21000; c++)
 			{
-				int d = GetRandInt(89) + 1;
-
-				Points[d] = (Points[d - 1] + Points[d + 1]) / 2.0;
-				Points[d] /= GetDistance(Points[d]);
+				for (int d = 1; d <= 89; d++)
+				{
+					Points[d] = (Points[d - 1] + Points[d + 1]) / 2.0;
+					Points[d] /= GetDistance(Points[d]);
+				}
 			}
 
 			for (int d = 0; d <= 90; d++)
 			{
-				Console.WriteLine(string.Format("Cos {0} degree is {1:F15}", d, Points[d].X));
-				Console.WriteLine(string.Format("Sin {0} degree is {1:F15}", d, Points[d].Y));
+				Console.WriteLine(string.Format("Cos {0} degree is {1:F12}", d, Points[d].X));
+				Console.WriteLine(string.Format("Sin {0} degree is {1:F12}", d, Points[d].Y));
+
+				//Console.WriteLine("Cos " + d + " : " + (Points[d].X - Math.Cos(d * Math.PI / 180.0)).ToString("F13"));
+				//Console.WriteLine("Sin " + d + " : " + (Points[d].Y - Math.Sin(d * Math.PI / 180.0)).ToString("F13"));
+
+				//Console.WriteLine("Cos " + d + " : " + (Points[d].X - Math.Cos(d * Math.PI / 180.0)).ToString("F12"));
+				//Console.WriteLine("Sin " + d + " : " + (Points[d].Y - Math.Sin(d * Math.PI / 180.0)).ToString("F12"));
 			}
 		}
 
@@ -39,31 +44,5 @@ namespace Charlotte.Tests
 		{
 			return Math.Sqrt(pt.X * pt.X + pt.Y * pt.Y);
 		}
-
-		// ====
-		// Random
-		// ====
-
-		public static ulong RndCtr = 1;
-
-		public static ulong GetRandULong() // Xorshift-64
-		{
-			RndCtr ^= RndCtr << 13;
-			RndCtr ^= RndCtr >> 7;
-			RndCtr ^= RndCtr << 17;
-			return RndCtr;
-		}
-
-		public static int GetRandInt(int modulo)
-		{
-			return (int)(GetRandULong() % (ulong)modulo);
-		}
-
-		public static double GetRandReal() // ret: 0.0 ～ 1.0
-		{
-			return (double)GetRandInt(int.MaxValue) / (int.MaxValue - 1);
-		}
-
-		// ====
 	}
 }
