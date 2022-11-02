4eb2b8ac67e028fb48bef76cbf342c97f7cb6cf6
soleil.taruto (cerulean.charlotte) <soleil.taruto@gmail.com>
2022-11-02 23:12:05 +0900

    ss

--- Dev/Program/Test0001/Claes20200001/Claes20200001/Tests/Test0004.cs
+++ Dev/Program/Test0001/Claes20200001/Claes20200001/Tests/Test0004.cs
@@ -21,19 +21,21 @@ namespace Charlotte.Tests
 				{
 					Points[d] = (Points[d - 1] + Points[d + 1]) / 2.0;
 					Points[d] /= GetDistance(Points[d]);
+					//Points[d].X = Math.Sqrt(1.0 - Points[d].Y * Points[d].Y);
+					//Points[d].Y = Math.Sqrt(1.0 - Points[d].X * Points[d].X);
 				}
 			}
 
 			for (int d = 0; d <= 90; d++)
 			{
-				Console.WriteLine(string.Format("Cos {0} degree is {1:F12}", d, Points[d].X));
-				Console.WriteLine(string.Format("Sin {0} degree is {1:F12}", d, Points[d].Y));
-
-				//Console.WriteLine("Cos " + d + " : " + (Points[d].X - Math.Cos(d * Math.PI / 180.0)).ToString("F13"));
-				//Console.WriteLine("Sin " + d + " : " + (Points[d].Y - Math.Sin(d * Math.PI / 180.0)).ToString("F13"));
+				//Console.WriteLine(string.Format("Cos {0} degree is {1:F12}", d, Points[d].X));
+				//Console.WriteLine(string.Format("Sin {0} degree is {1:F12}", d, Points[d].Y));
 
 				//Console.WriteLine("Cos " + d + " : " + (Points[d].X - Math.Cos(d * Math.PI / 180.0)).ToString("F12"));
 				//Console.WriteLine("Sin " + d + " : " + (Points[d].Y - Math.Sin(d * Math.PI / 180.0)).ToString("F12"));
+
+				Console.WriteLine("Cos " + d + " : " + (Points[d].X - Math.Cos(d * Math.PI / 180.0)).ToString("F13"));
+				Console.WriteLine("Sin " + d + " : " + (Points[d].Y - Math.Sin(d * Math.PI / 180.0)).ToString("F13"));
 			}
 		}
 
---
+++ Dev/Program/Test0001/Claes20200001/Claes20200001/Tests/Test0005.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Charlotte.Utilities;
+
+namespace Charlotte.Tests
+{
+	public class Test0005
+	{
+		//private const int RANGE_SCALE = 10000;
+		//private const int RANGE_SCALE = 100000;
+		//private const int RANGE_SCALE = 1000000;
+		private const int RANGE_SCALE = 10000000;
+		//private const int RANGE_SCALE = 100000000;
+		//private const int RANGE_SCALE = 1000000000;
+
+		private static int[] Primes =
+			Enumerable.Range(0, RANGE_SCALE).Where(n => IsPrime((ulong)n))
+			.Concat(new int[] {
+			Enumerable.Range(RANGE_SCALE, int.MaxValue - RANGE_SCALE).First(n => IsPrime((ulong)n)) })
+			.ToArray();
+
+		public void Test01()
+		{
+			for (int k = 1; k <= 100; k++)
+				PrintPrimeVoid(k);
+
+		}
+
+		private static void PrintPrimeVoid(int k)
+		{
+			Console.WriteLine(string.Format("2 以上 {0} 以下の異なる 2 つの素数についてその間にある素数の個数が {1} 未満のもののうち差が最大となるものは：", Primes[Primes.Length - 1], k));
+
+			int maxDiff = Enumerable.Range(0, Primes.Length - k).Select(i => Primes[i + k] - Primes[i]).Max();
+
+			foreach (int i in Enumerable.Range(0, Primes.Length - k))
+				if (maxDiff == Primes[i + k] - Primes[i])
+					Console.WriteLine(string.Format("{0} と {1} その差は {2}", Primes[i], Primes[i + k], Primes[i + k] - Primes[i]));
+
+			Console.WriteLine("");
+		}
+
+		public static bool IsPrime(ulong n)
+		{
+			return MillerRabin.IsPrime(n);
+		}
+	}
+}
