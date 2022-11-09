48a67bcbb72b0474810b67c793303e776199ff5c
soleil.taruto (cerulean.charlotte) <soleil.taruto@gmail.com>
2022-11-09 19:47:46 +0900

    ss

--- Dev/Program/Test0001/Claes20200001/Claes20200001/Program.cs
+++ Dev/Program/Test0001/Claes20200001/Claes20200001/Program.cs
@@ -54,7 +54,7 @@ namespace Charlotte
 		{
 			// -- choose one --
 
-			new Test0001().Test01();
+			new Test0001().Test01(); // unsafe
 			//new Test0002().Test01();
 			//new Test0003().Test01();
 
