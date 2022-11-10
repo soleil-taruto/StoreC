5c99c9b5cd3a5d93812329e76735e094105fa1a6
soleil.taruto (cerulean.charlotte) <soleil.taruto@gmail.com>
2022-11-11 00:07:03 +0900

    ss

--- Dev/Program/Test0001/Claes20200001/Claes20200001/Tests/Test0002.cs
+++ Dev/Program/Test0001/Claes20200001/Claes20200001/Tests/Test0002.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.IO;
+using Charlotte.Commons;
 
 namespace Charlotte.Tests
 {
@@ -9,7 +11,42 @@ namespace Charlotte.Tests
 	{
 		public void Test01()
 		{
-			// none
+			string RES_TARGET_EXTS = @"
+
+.html
+.c
+.bat
+.conf
+.txt
+.js
+.cs
+
+";
+
+			string[] TARGET_EXTS = SCommon.TextToLines(RES_TARGET_EXTS).Select(v => v.Trim()).Where(v => v != "").ToArray();
+
+			List<string> tokens = new List<string>();
+
+			foreach (string file in Directory.GetFiles(@"C:\home\GitHub\Store\DevOld", "*", SearchOption.AllDirectories))
+			{
+				string ext = Path.GetExtension(file).ToLower();
+
+				if (TARGET_EXTS.Contains(ext))
+				{
+					Console.WriteLine("< " + file); // cout
+
+					Encoding encoding = ext == ".cs" ?
+						Encoding.UTF8 :
+						SCommon.ENCODING_SJIS;
+
+					string text = File.ReadAllText(file, encoding);
+
+					tokens.AddRange(SCommon.Tokenize(text, "\r\n\t 　", false, true));
+				}
+			}
+
+			File.WriteAllLines(@"C:\temp\1.txt", tokens, Encoding.UTF8);
+			File.WriteAllLines(@"C:\temp\2.txt", tokens.DistinctOrderBy(SCommon.Comp), Encoding.UTF8);
 		}
 	}
 }
