72d0f6b2eae1ed029b5d8f20c7fa56fe01657835
soleil.taruto (cerulean.charlotte) <soleil.taruto@gmail.com>
2022-11-18 08:39:49 +0900

    Voyager 1 is 23667111291.341 kilometers away from the Sun.

---
+++ Dev/Program/Test20221111/Claes20200001/Claes20200001/Tests/Test0005.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace Charlotte.Tests
+{
+	public class Test0005
+	{
+		public void Test01()
+		{
+			const int ARR_LEN = 30000000;
+
+			{
+				int[] arr = Enumerable.Range(1, ARR_LEN).ToArray();
+
+				PrintExecuteTime(() => Console.WriteLine(arr.Count())); // 0.0s
+				PrintExecuteTime(() => Console.WriteLine(arr.Count())); // 0.0s
+				PrintExecuteTime(() => Console.WriteLine(arr.Count())); // 0.0s
+			}
+
+			{
+				List<int> list = new List<int>();
+
+				foreach (int value in Enumerable.Range(1, ARR_LEN))
+					list.Add(value);
+
+				PrintExecuteTime(() => Console.WriteLine(list.Count())); // 0.0s
+				PrintExecuteTime(() => Console.WriteLine(list.Count())); // 0.0s
+				PrintExecuteTime(() => Console.WriteLine(list.Count())); // 0.0s
+			}
+
+			{
+				int[] arr = Enumerable.Range(1, ARR_LEN).ToArray();
+
+				IEnumerable<int> enu = arr.Take(arr.Length);
+
+				PrintExecuteTime(() => Console.WriteLine(enu.Count())); // 0.2s
+				PrintExecuteTime(() => Console.WriteLine(enu.Count())); // 0.2s
+				PrintExecuteTime(() => Console.WriteLine(enu.Count())); // 0.2s
+			}
+		}
+
+		private static void PrintExecuteTime(Action routine)
+		{
+			DateTime stTm = DateTime.Now;
+			routine();
+			DateTime edTm = DateTime.Now;
+			Console.WriteLine((edTm - stTm).TotalSeconds.ToString("F9") + " 秒");
+		}
+	}
+}
