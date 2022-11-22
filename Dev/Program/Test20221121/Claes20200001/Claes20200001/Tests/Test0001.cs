4bf78d913a2d1980de3afaa9e696fd85df5a799d
soleil.taruto (cerulean.charlotte) <soleil.taruto@gmail.com>
2022-11-22 22:07:59 +0900

    Voyager 1 is 23673767999.120 kilometers away from the Sun.

--- Dev/Program/Test20221121/Claes20200001/Claes20200001/Tests/Test0001.cs
+++ Dev/Program/Test20221121/Claes20200001/Claes20200001/Tests/Test0001.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Text;
 using System.Drawing;
 using Charlotte.Commons;
+using System.IO;
+using Charlotte.Utilities;
 
 namespace Charlotte.Tests
 {
@@ -11,7 +13,62 @@ namespace Charlotte.Tests
 	{
 		public void Test01()
 		{
-			// none
+			Test01_a(@"C:\temp\2022-11-22_16-49-06_000.jpg");
+		}
+
+		private void Test01_a(string file)
+		{
+			Canvas canvas = Canvas.LoadFromFile(file);
+
+			canvas.FilterAllDot(dot =>
+			{
+				dot = new I4Color(
+					255 - (255 - dot.R) / 2,
+					255 - (255 - dot.G) / 2,
+					255 - (255 - dot.B) / 2,
+					dot.A
+					);
+
+				return dot;
+			});
+
+			canvas.Save(SCommon.NextOutputPath() + ".png");
+
+			// ----
+
+			canvas = Canvas.LoadFromFile(file);
+
+			canvas.FilterAllDot(dot =>
+			{
+				dot = new I4Color(
+					Math.Min(255, dot.R * 2),
+					Math.Min(255, dot.G * 2),
+					Math.Min(255, dot.B * 2),
+					dot.A
+					);
+
+				return dot;
+			});
+
+			canvas.Save(SCommon.NextOutputPath() + ".png");
+
+			// ----
+
+			canvas = Canvas.LoadFromFile(file);
+
+			canvas.FilterAllDot(dot =>
+			{
+				dot = new I4Color(
+					Math.Min(255, dot.R * 4),
+					Math.Min(255, dot.G * 4),
+					Math.Min(255, dot.B * 4),
+					dot.A
+					);
+
+				return dot;
+			});
+
+			canvas.Save(SCommon.NextOutputPath() + ".png");
 		}
 	}
 }
