7188890bc3f53b29606be51da4eb43058fb3e145
soleil.taruto (cerulean.charlotte) <soleil.taruto@gmail.com>
2022-11-28 05:15:20 +0900

    Voyager 1 is 23681498299.922 kilometers away from the Sun.

--- Dev/GameJS/LiteTemplate/Gattonero20200001/_index_Release.html.js
+++ Dev/GameJS/LiteTemplate/Gattonero20200001/_index_Release.html.js
@@ -35,6 +35,23 @@ window.onload = function()
 
 <br/>
 
+素材：<br/>
+
+<br/>
+
+音楽<br/>
+DOVA-SYNDROME<br/>
+https://dova-s.jp/<br/>
+
+<br/>
+
+効果音<br/>
+小森平<br/>
+https://taira-komori.jpn.org/<br/>
+
+<br/>
+<br/>
+
 操作方法：<br/>
 ここに操作方法を書くあああああいいいいうううう①<br/>
 ここに操作方法を書くあああああいいいいうううう②<br/>
